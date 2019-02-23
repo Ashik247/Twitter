15720b96b9bbf9f0673b1f4426f0f861f6f3b188
Ashik Chowdhury <zero@ashiks-macbook-pro.local>
2019-02-23 05:22:46 -0500

    cell connections

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Ashik Chowdhury on 2/23/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    @IBOutlet weak var tweetImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
